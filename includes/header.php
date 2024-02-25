26a05cb2bffdcc8a119253bd57808fb4bae0aa07
chanez begrar <110905077+chanezbegrar@users.noreply.github.com>
2024-02-25 11:30:28 +0100

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,47 @@
+<nav class="navbar navbar-expand-lg bg-light fixed-top shadow-lg">
+                <div class="container">
+                    <a class="navbar-brand mx-auto d-lg-none" href="index.php">
+                        Rendez-Vous chez L'Orthophoniste
+                        <strong class="d-block">Logiciel de gestion</strong>
+                    </a>
+
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav mx-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="index.php">Home</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#about">About</a>
+                            </li>
+
+                           
+
+                            <a class="navbar-brand d-none d-lg-block" href="index.php">
+                            Rendez-Vous chez L'Orthophoniste
+                                <strong class="d-block">Logiciel de gestion</strong>
+                            </a>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="check-appointment.php">Check Appointment</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#booking">Booking</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#contact">Contact</a>
+                            </li>
+                             <li class="nav-item active">
+                                <a class="nav-link" href="doctor/login.php">Connecter</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+            </nav>
